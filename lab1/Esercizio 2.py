c2857574a617b5177bede7e792bb44e586bc2bc0
filippoLonghi <filippo.longhi.01@gmail.com>
2022-03-09 11:12:56 +0100

    esercizio 2 test aggiunto

---
+++ lab1/Esercizio 2.py
@@ -0,0 +1,9 @@
+stringa1 = input("inserire la prima stringa: ")
+stringa2 = input("inserire la seconda stringa: ")
+
+stringa1_insieme = {}
+stringa2_insieme = {}
+alfabeto = ""
+
+print("ciao")
+#for ch1 in stringa1:
