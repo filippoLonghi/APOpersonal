682a7fce1d0ef9502b55ffd1298dcc8ddda7337f
filippoLonghi <filippo.longhi.01@gmail.com>
2022-03-11 11:22:49 +0100

    aggiunto strategia militare

---
+++ lab2/Strategia militare.py
@@ -0,0 +1,6 @@
+#creo una tabella con i dati nel foglio di testo
+#itero sulla tabella
+#larghezza: numero di 1 nella tabella
+#numero di file: numero maggiore nella tabella
+#direzione: vedere quale numero è maggiore di quello attuale nelle quattro posizioni adiacenti
+#fila con più buchi: fai un contatore che ti conta quanti numeri ci sono per ogni fila, il minore è la fila con più buchi
