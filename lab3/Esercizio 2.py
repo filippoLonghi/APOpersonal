1607c280132dd892bf0e88c19a957b288dce1e5e
filippoLonghi <filippo.longhi.01@gmail.com>
2022-03-18 12:27:40 +0100

    luca pizza

--- lab3/Esercizio 2.py
+++ lab3/Esercizio 2.py
@@ -4,12 +4,20 @@ from tkinter import ttk
 root = Tk()
 root.title("Esercizio 1")
 
-la = ttk.Label(root, text='questo spazio cambierà colore')
+def funzione(*args):
+    if lb.select_includes(0):
+        la["background"] = "red"
+    elif lb.select_includes(1):
+        la["background"] = "blue"
+    elif lb.select_includes(2):
+        la["background"] = "yellow"
+    elif lb.select_includes(3):
+        la["background"] = "white"'''
 
 colori = ["red", "blue", "yellow", "white"]
 coloriVar = StringVar(value=colori)
 lb = Listbox(root, listvariable=coloriVar, height=4)
+lb.bind('<Button-1>', funzione)
 
 lb.pack()
-la.pack()
 root.mainloop()
